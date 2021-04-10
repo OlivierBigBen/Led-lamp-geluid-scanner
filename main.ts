361c21a184dbb7b2d0f1a5d78e278ca326050742
OlivierBigBen <82178198+OlivierBigBen@users.noreply.github.com>
2021-04-10 10:28:24 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+let instel = 0
+let strip = neopixel.create(DigitalPin.P1, 30, NeoPixelMode.RGB)
+let _4digit = grove.createDisplay(DigitalPin.P2, DigitalPin.P16)
+basic.forever(function () {
+    _4digit.show(instel)
+})
+basic.forever(function () {
+    instel = pins.map(
+    pins.analogReadPin(AnalogPin.P0),
+    0,
+    1013,
+    1,
+    100
+    )
+    strip.showBarGraph(1, instel)
+})
